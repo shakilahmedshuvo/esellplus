65780d185e78a057449b588aebb3d93909d59b35
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-02 23:17:00 +0600

    add navbar logo

--- src/Page/HomePage/Navbar/Navbar.jsx
+++ src/Page/HomePage/Navbar/Navbar.jsx
@@ -9,17 +9,28 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto grid grid-cols-12">
 
                 {/* logo section start */}
-                <div className="flex">
+                <div className="col-span-2">
+                    <div className="flex">
 
-                    <img
-                        src={logo}
-                        alt="" />
+                        {/* <div className="flex"> */}
+                        <img
+                            className="h-[70%] mt-2"
+                            src={logo}
+                            alt="" />
+                        {/* </div> */}
 
-                    <div>
-                        <h2 className="text-3xl font-semibold">
-                            ESEllPLUS
-                        </h2>
+                        <div>
+                            <h2
+                                className="text-2xl font-semibold text-[#223994] flex items-center justify-center pt-8 pl-3">
+                                ESEllPLUS
+                                {/* ESellPlus */}
+                            </h2>
+
+                        </div>
                     </div>
+                    <p className="text-xs text-gray-400 ml-1">
+                        Online Fashion Shopping Center
+                    </p>
                 </div>
                 {/* logo section end */}
 
