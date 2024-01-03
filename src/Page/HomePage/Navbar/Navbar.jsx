101ee1f3fc0887619ebf2eab7338911dca1712c5
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-03 12:46:57 +0600

    add location section

--- src/Page/HomePage/Navbar/Navbar.jsx
+++ src/Page/HomePage/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import TopNavbar from "./TopNavbar";
 import logo from "../../../../public/logo.png";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
     return (
@@ -34,9 +35,41 @@ const Navbar = () => {
                 </div>
                 {/* logo section end */}
 
+
+                {/* drop down section start */}
+                    <div className="dropdown w-full h-fit col-span-2 border rounded-md mt-4">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="flex items-center justify-between px-5 py-3">
+
+                            <div>
+                                <p className="text-xs text-gray-300">
+                                    Your Location
+                                </p>
+                                <h2 className="text-[#223994] font-medium">
+                                    Your Location
+                                </h2>
+                            </div>
+                            <IoIosArrowDown className="text-xl" />
+
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="dropdown-content z-[1] menu p-2 shadow bg-gray-50 rounded-box w-52 font-medium">
+                            <li><a>Dhaka</a></li>
+                            <li><a>Gazipur</a></li>
+                            <li><a>Norshindi</a></li>
+                            <li><a>Sylhet</a></li>
+                            <li><a>Noakhali</a></li>
+                            <li><a>Borishal</a></li>
+                        </ul>
+                    </div>
+                </div>
+                {/* drop down section end */}
+
             </div>
 
-        </div>
     );
 };
 
