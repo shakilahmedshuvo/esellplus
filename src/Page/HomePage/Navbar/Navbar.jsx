228f401e491b7d103b0e77c37f9a8ebbd0f0b1c5
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-04 12:51:19 +0600

    add navbar section

--- src/Page/HomePage/Navbar/Navbar.jsx
+++ src/Page/HomePage/Navbar/Navbar.jsx
@@ -3,8 +3,7 @@ import logo from "../../../../public/logo.png";
 import { IoIosArrowDown } from "react-icons/io";
 import { RiSearchLine } from "react-icons/ri";
 import { LuUser2 } from "react-icons/lu";
-
-
+import { IoBagHandleOutline } from "react-icons/io5";
 
 const Navbar = () => {
     return (
@@ -14,7 +13,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto grid grid-cols-12">
 
                 {/* logo section start */}
-                <div className="col-span-2">
+                <div className="col-span-2 cursor-pointer">
                     <div className="flex">
 
                         {/* <div className="flex"> */}
@@ -41,7 +40,7 @@ const Navbar = () => {
 
 
                 {/* drop down section start */}
-                <div className="dropdown w-full h-fit col-span-2 border rounded-md mt-4 border-gray-300">
+                <div className="dropdown w-full h-fit col-span-2 border rounded-lg mt-4 border-gray-300">
                     <div
                         tabIndex={0}
                         role="button"
@@ -74,9 +73,9 @@ const Navbar = () => {
                 {/* input section start */}
                 <div className="col-span-6">
                     <form
-                        className="grid grid-cols-12 ms-10 bg-[#F3F4F7] mt-4 rounded-md">
+                        className="grid grid-cols-12 ms-10 bg-[#F3F4F7] mt-4 rounded-lg">
                         <input
-                            className="w-full bg-[#F3F4F7] py-5 ps-10 col-span-11 focus:outline-none"
+                            className="w-full bg-[#F3F4F7] py-5 ps-10 col-span-11 focus:outline-none rounded-lg"
                             placeholder="Search for products"
                             name="search"
                             type="text" />
@@ -90,10 +89,10 @@ const Navbar = () => {
                 {/* input section end */}
 
                 {/* user section start */}
-                <div className="col-span-2 flex items-center">
+                <div className="col-span-2 flex items-center justify-between">
 
                     {/* user profile section start */}
-                    <div className="mt-7 m-6">
+                    <div className="mt-7 ms-6 mb-6">
                         <LuUser2 className="text-4xl p-2 border-2 rounded-full" />
                     </div>
                     {/* user profile section end */}
@@ -105,7 +104,13 @@ const Navbar = () => {
                     {/* total dollar count section end */}
 
                     {/* user cart section start */}
-                    <div>
+                    <div className="flex items-center justify-center">
+
+                        <IoBagHandleOutline className="text-5xl bg-[#FFF1EE] text-[#EA2B0F] p-3 rounded-full" />
+                        <p
+                            className="relative right-4 bottom-4 text-white bg-[#EA2B0F] p-2 rounded-full w-6 h-6 flex items-center justify-center text-xs">
+                            0
+                        </p>
 
                     </div>
                     {/* user cart section end */}
