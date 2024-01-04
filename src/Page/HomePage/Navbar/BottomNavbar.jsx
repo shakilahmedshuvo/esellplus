139be1a64c6dcf303701eeaf737b6a97a29fca06
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-04 15:32:05 +0600

    add product section

--- src/Page/HomePage/Navbar/BottomNavbar.jsx
+++ src/Page/HomePage/Navbar/BottomNavbar.jsx
@@ -1,9 +1,38 @@
+import { IoIosArrowDown } from "react-icons/io";
+import { VscMenu } from "react-icons/vsc";
+
 const BottomNavbar = () => {
     return (
-        <div className="max-w-7xl mx-auto grid grid-cols-12 mt-3">
+        <div className="max-w-7xl mx-auto grid grid-cols-12 mt-5">
 
-            <div className="col-span-2">
+            <div className="col-span-3">
+                <div className="dropdown dropdown-hover w-[73%]">
+                    <div
+                        tabIndex={0}
+                        role="button">
 
+                        <div
+                            className="bg-[#2BBEF9] text-white flex items-center rounded-full py-3 w-full">
+                            <VscMenu className="text-base ms-6 my-1" /><p className="mt-1 text-sm font-medium ms-4">ALL CATEGORIES</p><IoIosArrowDown className="my-1 ms-8 text-lg" />
+                        </div>
+
+                        <div className="flex items-center justify-center">
+                            <p className="relative bottom-2 text-[10px] bg-[#EDEEF5] w-fit font-medium px-3 rounded-full text-gray-500 mr-4">
+                                TOTAL 100+ PRODUCTS
+                            </p>
+                        </div>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a>Item 1</a>
+                        </li>
+                        <li>
+                            <a>Item 2</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <div className="col-span-10">
