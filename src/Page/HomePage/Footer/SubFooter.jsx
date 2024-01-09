3f04b8f6415010eb4d27751bfebab08f08ac1935
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-09 17:56:24 +0600

    add sub footer section deisgn

--- src/Page/HomePage/Footer/SubFooter.jsx
+++ src/Page/HomePage/Footer/SubFooter.jsx
@@ -1,7 +1,38 @@
+import { LiaPhoneVolumeSolid } from "react-icons/lia";
+
 const SubFooter = () => {
     return (
-        <div>
-            
+        <div className="max-w-7xl mx-auto pt-5 flex justify-between">
+
+            <div className="flex items-center">
+                <div
+                    className="text-2xl rounded-full border p-2 w-fit flex items-center justify-center">
+                    <LiaPhoneVolumeSolid />
+                </div>
+
+                <div className="ms-4">
+                    <h2 className="text-2xl font-medium pt-2">
+                        8 800 555-55
+                    </h2>
+                    <p className="text-xs text-gray-400">
+                        Working on 8:00 - 22:00
+                    </p>
+                </div>
+            </div>
+
+
+            <div className="grid grid-cols-3">
+                <div>
+                    <h2 className="font-medium pt-2">
+                        Download App on Mobile :
+                    </h2>
+                    <p className="text-xs text-gray-400">
+                        15% discount on your first purchase
+                    </p>
+                </div>
+
+            </div>
+
         </div>
     );
 };
