abc091b10c1329a64329e426297fcbe78f5c1453
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-17 12:32:37 +0600

    deal of the week left section design

--- src/Page/HomePage/DealOfTheWeek/LeftDealOfTheWeek.jsx
+++ src/Page/HomePage/DealOfTheWeek/LeftDealOfTheWeek.jsx
@@ -16,11 +16,11 @@ const LeftDealOfTheWeek = () => {
                 product.slice(0, 6).map(item => (
                     // single card section start
                     <div
-                        className="border border-gray-200"
+                        className="border border-gray-100"
                         key={item?.id}>
 
                         {/* img section start */}
-                        <div className="mx-5 mt-4">
+                        <div className="mx-5 mt-5">
                             <h2
                                 className="bg-[#2BBEF9] font-medium text-white text-center w-fit text-xs px-3 py-1 rounded-md">
                                 {item?.percentage}%
@@ -34,25 +34,40 @@ const LeftDealOfTheWeek = () => {
                             </div>
 
 
-                            <h2
-                                className={`${item.recommended ? 'relative bottom-32 bg-[#71778E] font-medium w-fit text-xs text-white py-1 px-2 rounded-md' : ''}`}>
-                                {
-                                    item.recommended ?
-                                        'RECOMMENDED' : ''
-                                }
-                            </h2>
+                            <div className="relative">
+                                <h2
+                                    className={`${item.recommended ? 'absolute bottom-32 bg-[#71778E] font-medium w-fit text-[9px] text-white py-1 px-2 rounded-md' : ''}`}>
+                                    {
+                                        item.recommended ?
+                                            'RECOMMENDED' : ''
+                                    }
+                                </h2>
+                            </div>
 
 
                         </div>
                         {/* img section end */}
 
-                        {/* <p
-                            className={`${item.inStock ? 'text-green-500' : 'text-red-500'}`}>
-                            {
-                                item.inStock ?
-                                'In Stock' : 'Out of Stock'
-                            }
-                        </p> */}
+                        {/* details section start */}
+                        <div className="mx-6">
+
+                            <h2 className="text-gray-600">
+                                {item?.name}
+                            </h2>
+
+
+                            <p
+                                className={`${item.inStock ? 'text-green-500 text-xs font-medium my-1' : 'text-red-500 text-xs font-medium my-1'}`}>
+                                {
+                                    item.inStock ?
+                                        'In Stock' : 'Out of Stock'
+                                }
+                            </p>
+
+
+                        </div>
+                        {/* details section end */}
+
                     </div>
                     // single card section end
                 ))
